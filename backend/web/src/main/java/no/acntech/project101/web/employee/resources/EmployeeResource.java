867efc5e122548b58abf8fd6d608760e9aab8aab
Ellisiv Steen <ellisiv_s_s@hotmail.com>
2021-08-31 05:38:05 +0200

    Created working EndPoints for Employees

--- backend/web/src/main/java/no/acntech/project101/web/employee/resources/EmployeeResource.java
+++ backend/web/src/main/java/no/acntech/project101/web/employee/resources/EmployeeResource.java
@@ -1,41 +1,76 @@
 package no.acntech.project101.web.employee.resources;
 
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import no.acntech.project101.employee.Employee;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins = "http://localhost:3000")
 //TODO This is a REST controler and should receive request on path employees
+@RestController
+@RequestMapping("employees")
 public class EmployeeResource {
 
     //TODO The constructor needs to accept the required dependencies and assign them to class variables
     public EmployeeResource() {
+
     }
 
+    @GetMapping
     public ResponseEntity<List<EmployeeDto>> findAll() {
         //TODO create a GET endpoint find all employees in the database and return them
-        return null;
-    }
+        final EmployeeDto ellisiv = new EmployeeDto(1L, "Ellisiv", "Steen", LocalDate.of(1997, 4, 18), 1L);
+        final EmployeeDto solfrid = new EmployeeDto(2L, "Solfrid", "Johansen", LocalDate.of(1997, 2, 6), 1L);
+        final List<EmployeeDto> employees = Arrays.asList(ellisiv, solfrid);
 
-    public ResponseEntity<EmployeeDto> findById() {
+        return ResponseEntity.ok(employees);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> findById(@PathVariable final Long id) {
         // TODO create a GET endpoint that fetches a spesific employee based on its ID
-        return null;
-    }
+        final EmployeeDto employeeById = new EmployeeDto(1234L, "John", "Do", LocalDate.of(1900, 01, 01), 40L);
+        if (employeeById.getId().equals(id)) {
+            return ResponseEntity.ok(employeeById);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
 
-    public ResponseEntity createEmployee() {
+    }
+    @PostMapping
+    public ResponseEntity createEmployee(@RequestBody final EmployeeDto employeeDto) {
         //TODO Create a POST endpoint that accepts an employeeDTO and saves it in the database
-
-        return null;
+        final URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(employeeDto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 
-    public ResponseEntity deleteEmployee() {
+    @DeleteMapping("{id}")
+    public ResponseEntity deleteEmployee(@PathVariable final Long id) {
         // TODO Create a DELETE endpoint that deletes a specific employee
-        return null;
+        final EmployeeDto employeeById = new EmployeeDto(1234L, "John", "Do", LocalDate.of(1900, 01, 01), 40L);
+        if (employeeById.getId().equals(id)) {
+            return ResponseEntity.accepted().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    public ResponseEntity updateEmployee() {
+    @PatchMapping("{id}")
+    public ResponseEntity updateEmployee(@PathVariable final Long id, @RequestBody EmployeeDto employeeDto) {
         //TODO Create a PATCH endpoint that updates an employee with new values
-        return null;
+        final EmployeeDto employeeById = new EmployeeDto(1234L, "John", "Do", LocalDate.of(1900, 01, 01), 40L);
+        if (employeeById.getId().equals(id)) {
+            return ResponseEntity.accepted().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
