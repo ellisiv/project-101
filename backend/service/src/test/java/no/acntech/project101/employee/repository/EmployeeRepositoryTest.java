e3cbc12ecd9799278925d976379a52b064be8310
Ellisiv Steen <ellisiv_s_s@hotmail.com>
2021-08-31 10:02:51 +0200

    changed test for employee to support NOT NULL

--- backend/service/src/test/java/no/acntech/project101/employee/repository/EmployeeRepositoryTest.java
+++ backend/service/src/test/java/no/acntech/project101/employee/repository/EmployeeRepositoryTest.java
@@ -1,28 +1,37 @@
 package no.acntech.project101.employee.repository;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import no.acntech.project101.company.config.CompanyDatabaseConfig;
 import no.acntech.project101.employee.Employee;
 import no.acntech.project101.employee.config.EmployeeDatabaseConfig;
+import org.springframework.test.context.ContextConfiguration;
 
-//@DataJpaTest
-//@Import({EmployeeDatabaseConfig.class, CompanyDatabaseConfig.class})
-//@ContextConfiguration(classes = EmployeeRepository.class)
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Import({EmployeeDatabaseConfig.class, CompanyDatabaseConfig.class})
+@ContextConfiguration(classes = EmployeeRepository.class)
 public class EmployeeRepositoryTest {
 
-    //    @Autowired
-    //    private EmployeeRepository repository;
+    @Autowired
+    private EmployeeRepository repository;
 
     @Test
     void save() {
         final Employee employee = new Employee();
-        //        final Employee savedEmployee = repository.save(employee);
-        //        assertThat(savedEmployee.getId()).isNotNull();
-        //        assertThat(savedEmployee.getFirstName()).isEqualTo(employee.getFirstName());
-        //        assertThat(savedEmployee.getLastName()).isEqualTo(employee.getLastName());
-        //        assertThat(savedEmployee.getDateOfBirth()).isEqualTo(employee.getDateOfBirth());
+        employee.setFirstName("Ellisiv");
+        employee.setLastName("Steen");
+        employee.setDateOfBirth(LocalDate.now());
+        final Employee savedEmployee = repository.save(employee);
+        assertThat(savedEmployee.getId()).isNotNull();
+        assertThat(savedEmployee.getFirstName()).isEqualTo(employee.getFirstName());
+        assertThat(savedEmployee.getLastName()).isEqualTo(employee.getLastName());
+        assertThat(savedEmployee.getDateOfBirth()).isEqualTo(employee.getDateOfBirth());
     }
 }
