29f15701fa3c2cde0ecf98b2001ecd558c730fb1
Ellisiv Steen <ellisiv_s_s@hotmail.com>
2021-08-31 09:58:04 +0200

    Created employee db and entity

--- backend/domain/src/main/java/no/acntech/project101/employee/Employee.java
+++ backend/domain/src/main/java/no/acntech/project101/employee/Employee.java
@@ -1,11 +1,60 @@
 package no.acntech.project101.employee;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
 public class Employee {
 
     //TODO Create the enitity for Employee
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dateOfBirth;
 
     public Employee() {
         // Hibernate
     }
 
+    public Employee(final String firstName, final String lastName, final LocalDate dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
 }
