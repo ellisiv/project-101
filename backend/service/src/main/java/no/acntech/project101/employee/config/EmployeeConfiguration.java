8d95dd8e13ff94bff9fc7e2d3f10bc645f441517
Ellisiv Steen <ellisiv_s_s@hotmail.com>
2021-08-31 10:01:56 +0200

    added services to employee, config with db

--- backend/service/src/main/java/no/acntech/project101/employee/config/EmployeeConfiguration.java
+++ backend/service/src/main/java/no/acntech/project101/employee/config/EmployeeConfiguration.java
@@ -1,6 +1,14 @@
 package no.acntech.project101.employee.config;
 
-//TODO Add componentScan of employee package so that Spring can create beans
+//Add componentScan of employee package so that Spring can create beans
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@Configuration
+@ComponentScan(basePackages = "no.acntech.project101.employee")
 public class EmployeeConfiguration {
 
 }
